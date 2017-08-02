bb719ff959bafd1334916626327e89c5f1de0f78
Prasanna Venkataraman (TMS) <prasanna.venkataraman@toyota.com>
2017-08-02 07:21:42 -0700

    1

---
+++ SFTPClient/SFTPClient/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using SFTPClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SFTPClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(SftpViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+ 
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ SFTPClient/SFTPClient/Models/SftpViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SFTPClient.Models
+{
+    public class SftpViewModel
+    {
+        [Required]
+        [Display(Name = "SFTP Host name")]
+        public string HostName { get; set; }
+
+        [Required]
+        [Display(Name = "Port")]
+        public int Port { get; set; }
+        
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+    }
+}
