3b85f9d3bc58c7ea3a4dedad51bbd351b9ca2343
Sakthi Kumar Mahalingam (TMS) <sakthikumar.mahalingam@toyota.com>
2018-01-20 20:09:48 +0530

    1

---
+++ SFTPClient/SFTPClient/Controllers/FileViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SFTPClient.Controllers
+{
+    internal class FileViewModel
+    {
+        public object FileName { get; set; }
+        public object IsDirectory { get; set; }
+        public object Path { get; set; }
+        public object Size { get; set; }
+    }
+}
