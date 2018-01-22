3f9dc7cc38003124dc57f79ccca95da1ade91083
Sakthi Kumar Mahalingam (TMS) <sakthikumar.mahalingam@toyota.com>
2018-01-22 16:01:08 +0530

    1

---
+++ SFTPClient/run-nunit - Copy.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" "C:\Users\hi\Source\Repos\SFTPClient\SFTPClient\SFTPClient.NUnit.Tests\SFTPClient.NUnit.Tests.csproj" /t:RunUnitTests
+
+pause "End"
